9f506dabae3eaa80f98f5d99dacc9768e47dddd3
Ibragim <97885427+Heilonng23@users.noreply.github.com>
2024-05-04 12:07:42 +0200

    last details applied and simplified

--- src/Scroll/Scroll.js
+++ src/Scroll/Scroll.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Scroll = (props) => {
     return (
-      <div style={{ overflowY: 'scroll', border: '0px solid black', height: '623px', scrollbarWidth: 'thin', scrollbarColor: 'rgba(0, 0, 0, 0.5) rgba(0, 0, 0, 0.1)', width: '100%', overflowScrolling: 'touch'}}>
+      <div style={{
+        overflowY: 'scroll',
+        border: '0px solid black',
+        height: '623px',
+        scrollbarWidth: 'auto',
+        scrollbarColor: 'rgba(0, 0, 0, 0.5) rgba(0, 0, 0, 0.1)',
+        width: '100%',
+        overflowScrolling: 'auto',
+        scrollbarTrackColor: 'rgba(0, 0, 0, 0.2)',
+        scrollbarFaceColor: 'rgba(0, 0, 0, 0.3)',
+        scrollbarHighlightColor: 'rgba(0, 0, 0, 0.4)',
+        scrollbarShadowColor: 'rgba(0, 0, 0, 0.5)',
+        scrollbarDarkShadowColor: 'rgba(0, 0, 0, 0.6)',
+        overflow: 'scroll'
+      }}>
         {props.children}
       </div>
     );
